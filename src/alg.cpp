013e858f90deaea83a6a9da3f021dd2d905585db
yaroslavstepanov <75724049+yaroslavstepanov@users.noreply.github.com>
2021-05-27 17:00:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,35 +4,26 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
+
 BST<std::string> makeTree(const char* filename) {
-ifstream file;
-file.open(filename);
-BST<string> TREE;
-string word;
-string line;
-while (file)
-{
-file >> word;
-int i = 0;
-while (i < word.size())
-{
-int k = 0;
-while ((word[i] >= 'a' && word[i] <= 'z') || (word[i] >= 'A' && word[i] <= 'Z') && (i < word.size()))
-{
-if (word[i] >= 'A' && word[i] <= 'Z')
-word[i] += 32;
-line += word[i];
-i++;
-k++;
+BST<std::string>TREE;
+char FromAToa = 'a' - 'A';
+std::string Word = "";
+std::ifstream file(filename);
+if (!file) {
+std::cout << "File error!" << std::endl;
+return TREE;
 }
-if (k != 0)
-{
-TREE.add(line);
-line = "";
-}
-else
-i++;
+while (!file.eof()) {
+char Char = file.get();
+if ((Char >= 'a' && Char <= 'z') || (Char >= 'A' && Char <= 'Z')) {
+if (Char >= 'A' && Char <= 'Z')
+Char += FromAToa;
+Word += Char;
+} else if (Word != "") {
+TREE.add(Word);
 }
 }
+file.close();
 return TREE;
 }
